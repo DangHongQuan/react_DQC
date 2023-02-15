6bd350fe2d8eeae0db6f4a5ecc5fac6b670b485b
ADMIN <quandh21.cdsg@saigontech.edu.vn>
2023-02-15 08:12:33 +0700

    doen

--- src/components/DeaultLayoutAdmin.js
+++ src/components/DeaultLayoutAdmin.js
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from "react";
 import Admin from "./Admin";
 import { Routes, Route } from "react-router-dom";
 import routes from "./../routes";
+import AddSp from './../pages/AddSp';
 
 const DeaultLayoutAdmin = (props) => {
   return (
     <>
-          <Admin/>
+          <AddSp/>
           <Routes>
             {routes.map((route, idx) => (
               <Route key={idx} path={route.path} element={route.components} />
