949e5d873c386557a2e9c989b6b1d21705fc9b62
mr.chau <chauvo25102003@gmail.com>
2023-02-18 09:06:16 +0700

    delete

--- src/pages/Shopcart.js
+++ src/pages/Shopcart.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import  styled  from 'styled-components';
-import {cong, tru} from '../cart'
 
 const Shopcart = (props) => {
   const StyledCart = styled.div`
